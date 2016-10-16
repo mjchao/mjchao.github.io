d704e9768a68a4eabb066f621c90c7a7c5586f3c
Mickey Chao <mjchao2@gmail.com>
2016-10-16 18:25:55 -0400

    Add basic panels for website.

---
+++ js/index.js
@@ -0,0 +1,26 @@
+function HideAll() {
+	$("#pnl_menu").hide();
+	$("#pnl_projects").hide();
+	$("#pnl_skills").hide();
+	$("#pnl_timeline").hide();
+}
+
+function ShowMenu() {
+	HideAll();
+	$("#pnl_menu").show();
+}
+
+function ShowProjects() {
+	HideAll();
+	$("#pnl_projects").show();
+}
+
+function ShowSkills() {
+	HideAll();
+	$("#pnl_skills").show();
+}
+
+function ShowTimeline() {
+	HideAll();
+	$("#pnl_timeline").show();
+}
