69a0c992021a9160d393d4da9ce9254678950585
Mickey Chao <mjchao2@gmail.com>
2017-09-02 22:45:47 -0500

    Modify skills grid appearance.

--- js/skills.js
+++ js/skills.js
@@ -1,9 +1,4 @@
-function hasClass( elem, klass ) {
-       return (" " + elem.className + " " ).indexOf( " "+klass+" " ) > -1;
-}
-
 var skill_boxes = document.querySelectorAll("section.main aside");
-console.log(skill_boxes);
 for (var i = 0; i < skill_boxes.length; ++i) {
   var skill_box = skill_boxes[i];
   skill_box.addEventListener("click",
