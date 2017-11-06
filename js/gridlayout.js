0b7698858db8770a9974a721ecd6d74f795b6ab2
Mickey Chao <mjchao2@gmail.com>
2017-11-05 22:08:23 -0600

    Add in close button for gridlayout.

--- js/gridlayout.js
+++ js/gridlayout.js
@@ -1,9 +1,16 @@
+"use strict";
+
 var boxes = document.querySelectorAll("section.main aside");
 for (var i = 0; i < boxes.length; ++i) {
   var box = boxes[i];
   box.addEventListener("click",
-      function() {
-        this.classList.toggle("selected");
-      }
+    function() {
+      this.classList.toggle("selected");
+    }
   );
+
+  var close_button = document.createElement("a");
+  close_button.className = "close btn";
+  close_button.text = "close";
+  box.insertBefore( close_button, box.childNodes[0] );
 }
