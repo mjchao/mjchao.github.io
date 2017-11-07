50b8be43b4b663c93afad5355ed430068c8444c9
Mickey Chao <mjchao2@gmail.com>
2017-11-06 20:15:51 -0600

    Make close button less intrusive.

--- js/gridlayout.js
+++ js/gridlayout.js
@@ -4,13 +4,25 @@ var boxes = document.querySelectorAll("section.main aside");
 for (var i = 0; i < boxes.length; ++i) {
   var box = boxes[i];
   box.addEventListener("click",
-    function() {
-      this.classList.toggle("selected");
+    function(e) {
+      if (!e.target.classList.contains("close") &&
+          !this.classList.contains("selected")) {
+        this.classList.toggle("selected");
+      }
     }
   );
 
+  function BuildOnCloseButtonClicked(parent_box) {
+    return function() {
+      if (parent_box.classList.contains("selected")) {
+        parent_box.classList.toggle("selected");
+      }
+    }
+  }
+
   var close_button = document.createElement("a");
-  close_button.className = "close btn";
-  close_button.text = "close";
+  close_button.addEventListener("click", BuildOnCloseButtonClicked(box));
+  close_button.className = "close";
+  close_button.text = "\u00d7";
   box.insertBefore( close_button, box.childNodes[0] );
 }
