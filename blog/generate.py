058461a6344709123023d6ce4199d9e676ae8d5e
Mickey Chao <mjchao2@gmail.com>
2018-02-13 22:23:12 -0600

    Add related posts section.

--- blog/generate.py
+++ blog/generate.py
@@ -5,6 +5,7 @@ ROOT = "/"
 CURDIR = os.path.join(ROOT, "blog")
 BLOG_POST_TEMPLATE = "post.template"
 BLOG_SUMMARY_TEMPLATE = "summary.template"
+RELATED_POSTS_VAR = "RELATED"
 
 with open(BLOG_POST_TEMPLATE, "r") as f:
     post_template = f.read()
@@ -12,6 +13,7 @@ with open(BLOG_POST_TEMPLATE, "r") as f:
 with open(BLOG_SUMMARY_TEMPLATE, "r") as f:
     summary_template = f.read()
 
+blogdirs = set()
 for x in os.walk("."):
     subdir = x[0]
     # ignore current dir
@@ -20,24 +22,51 @@ for x in os.walk("."):
     if subdir.startswith("./"):
         subdir = subdir[2:]
 
-    with open("%s/vars.json" %(subdir)) as f:
+    blogdirs.add(subdir)
+
+for blogdir in blogdirs:
+    with open("%s/vars.json" %(blogdir)) as f:
         vars = json.load(f)
 
     # provide DIR variable by default, which is the directory in which the
     # blog data was stored.
-    vars["BLOGDIR"] = os.path.join(CURDIR, subdir)
+    vars["BLOGDIR"] = os.path.join(CURDIR, blogdir)
+
+    # Prevent substiting {{RELATED}} directly as a list. We want to
+    # substitute in the actual summaries, so that requires some special logic
+    # later.
+    if RELATED_POSTS_VAR in vars:
+        related_posts = vars[RELATED_POSTS_VAR]
+        del vars[RELATED_POSTS_VAR]
+    else:
+        related_posts = []
 
     # generate the blog summary
     summary_html = summary_template
     for k in vars:
         summary_html = summary_html.replace("{{%s}}" %(k), "%s" %(vars[k]))
-    with open("%s.summary.html" %(subdir), "w") as f:
+
+    with open("%s.summary.html" %(blogdir), "w") as f:
         f.write(summary_html)
 
     # generate the blog content
     post_html = post_template
     for k in vars:
         post_html = post_html.replace("{{%s}}" %(k), "%s" %(vars[k]))
-    with open("%s.html" %(subdir), "w") as f:
+
+    # add summaries of related posts to the bottom of the blog post
+    related_posts_html = ""
+    for related in related_posts:
+        if related not in blogdirs:
+            print ("ERR: %s in %s.RELATED is not a valid post. Ignoring..."
+                    %(related, blogdir))
+            continue
+        related_posts_html += (
+                "<div w3-include-html=\"%s.summary.html\"></div>\n"
+                %(os.path.join(CURDIR, related)))
+    post_html = post_html.replace("{{%s}}" %(RELATED_POSTS_VAR),
+            related_posts_html)
+
+    with open("%s.html" %(blogdir), "w") as f:
         f.write(post_html)
 
