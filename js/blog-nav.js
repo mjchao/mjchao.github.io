203c0494dee33a3a53187991f1b78e57072b68a9
Mickey Chao <mjchao2@gmail.com>
2018-04-08 22:18:02 -0500

    Turn nav into sidebar.

--- js/blog-nav.js
+++ js/blog-nav.js
@@ -1,6 +1,12 @@
 "use strict";
 
-function ToggleShowTOC() {
+function ToggleShowTOC(e) {
+  // prevent page from jumping to top when toc is toggled
+  e.preventDefault();
   var nav = document.querySelector("nav");
   nav.classList.toggle("toc-shown");
+
+  var body = document.querySelector("body");
+  body.classList.toggle("toc-shown");
 }
+
