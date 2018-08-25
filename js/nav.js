89db2ec46cdfb4cf385e29905563fcb9cffd0e99
Mickey Chao <mjchao2@gmail.com>
2018-08-25 14:08:13 -0500

    prevent mouse click default when clicking collasped nav.

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,9 @@
-function toggleCollapsedMenu() {
+function toggleCollapsedMenu(e) {
   var nav = document.getElementsByTagName("nav")[0];
   if (nav.className == "") {
     nav.className = "collapsed-showing";
   } else {
     nav.className = "";
   }
+  return false;
 }
